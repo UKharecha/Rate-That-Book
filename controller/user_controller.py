99029a12f16a820f3fc176d2ed3d002563839c8b
Umang Kharecha <72561940+UKharecha@users.noreply.github.com>
2024-09-12 12:51:08 +0530

    Update

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -16,11 +16,36 @@ def book_getall_controller():
     return obj.book_getall_model()
 
 
-@app.route("/user/addone", methods=["POST"])  # For Creating from Mysql
+@app.route("/user/addone", methods=["POST"])  # Add User
 def user_addone_controller():
     return obj.user_addone_model(request.form)
 
 
-@app.route("/book/addone", methods=["POST"])  # For Creating from Mysql
+@app.route("/book/addone", methods=["POST"])  # Add Book
 def book_addone_controller():
     return obj.book_addone_model(request.form)
+
+
+@app.route("/book/review/<id>", methods=["POST"])  # Add Book Review
+def book_addreview_controller():
+    return obj.book_addreview_model(request.form)
+
+
+@app.route("/user/update", methods=["PUT"])  # Update User
+def user_update_controller():
+    return obj.user_update_model(request.form)
+
+
+@app.route("/book/update", methods=["PUT"])  # Update Book
+def book_update_controller():
+    return obj.book_update_model(request.form)
+
+
+@app.route("/user/delete/<id>", methods=["DELETE"])  # Delete User
+def user_delete_controller(id):
+    return obj.user_delete_model(id)
+
+
+@app.route("/book/delete/<id>", methods=["DELETE"])  # Delete Book
+def book_delete_controller(id):
+    return obj.book_delete_model(id)
